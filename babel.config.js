38934eb43ac166f59021cc3d9fc8d725edf6ebd9
Maksym Klaban <maksym.klaban@gmail.com>
2023-01-18 16:12:57 +0200

    updated babel config

--- babel.config.js
+++ babel.config.js
@@ -3,8 +3,8 @@ module.exports = {
     development: {
       presets: ['@babel/preset-env', '@babel/preset-react'],
       plugins: [
-        ['babel-plugin-styled-components', { ssr: true, displayName: true, preprocess: false }],
-        // ['styled-components', { ssr: true, displayName: true, preprocess: false }],
+        // ['babel-plugin-styled-components'],
+        ['styled-components', { displayName: true, preprocess: false }],
         [
           'file-loader',
           {
@@ -16,13 +16,12 @@ module.exports = {
           },
         ],
       ],
-      babelrcRoots: ['.', 'packages/*'],
     },
     production: {
       presets: ['@babel/preset-env', '@babel/preset-react'],
       plugins: [
-        ['babel-plugin-styled-components', { ssr: true, displayName: true, preprocess: false }],
-        // ['styled-components', { ssr: true, displayName: true, preprocess: false }],
+        // ['babel-plugin-styled-components'],
+        ['styled-components', { displayName: true, preprocess: false }],
         [
           'file-loader',
           {
@@ -34,43 +33,6 @@ module.exports = {
           },
         ],
       ],
-      babelrcRoots: ['.', 'packages/*'],
-    },
-    server: {
-      presets: ['@babel/preset-env', '@babel/preset-react'],
-      plugins: [
-        ['babel-plugin-styled-components', { ssr: true, displayName: true, preprocess: false }],
-        // ['styled-components', { ssr: true, displayName: true, preprocess: false }],
-        [
-          'file-loader',
-          {
-            name: '[path][name].[ext]',
-            extensions: ['png', 'jpg', 'jpeg', 'gif', 'svg'],
-            outputPath: '/build',
-            context: './src',
-            limit: Infinity,
-          },
-        ],
-      ],
-      babelrcRoots: ['.', 'packages/*'],
     },
   },
-
-  // presets: ['next/babel', '@babel/preset-env', '@babel/preset-react'],
-  // plugins: [
-  //   ['babel-plugin-styled-components'],
-  //   ['styled-components', { ssr: true, displayName: true, preprocess: false }],
-  //   [
-  //     'file-loader',
-  //     {
-  //       name: '[path][name].[ext]',
-  //       extensions: ['png', 'jpg', 'jpeg', 'gif', 'svg'],
-  //       outputPath: '/build',
-  //       context: './src',
-  //       limit: Infinity,
-  //     },
-  //   ],
-  //   // ['styled-components', { ssr: true, displayName: true, preprocess: false }],
-  // ],
-  // babelrcRoots: ['.', 'packages/*'],
 }
