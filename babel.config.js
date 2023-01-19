9550bf2f526027e9d8c259e43f167669e32748d3
garagulya <alex.garagulya@gmail.com>
2023-01-19 16:47:21 +0200

    try diff styled components loader

--- babel.config.js
+++ babel.config.js
@@ -3,8 +3,8 @@ module.exports = {
     development: {
       presets: ['@babel/preset-env', '@babel/preset-react'],
       plugins: [
-        ['babel-plugin-styled-components', { ssr: false, displayName: true }],
-        // ['styled-components', { ssr: true, displayName: true, preprocess: false }],
+        // ['babel-plugin-styled-components', { ssr: false, displayName: true }],
+        ['styled-components', { ssr: false, displayName: true }],
         [
           'file-loader',
           {
@@ -20,8 +20,8 @@ module.exports = {
     production: {
       presets: ['@babel/preset-env', '@babel/preset-react'],
       plugins: [
-        ['babel-plugin-styled-components', { ssr: false, displayName: true }],
-        // ['styled-components', { ssr: true, displayName: true, preprocess: false }],
+        // ['babel-plugin-styled-components', { ssr: false, displayName: true }],
+        ['styled-components', { ssr: false, displayName: true }],
         [
           'file-loader',
           {
